8526601889e6225b856b5cd6d4d70bade52cb581
Nikhil Srivastava <niksrc@gmail.com>
2020-09-17 13:13:59 +0530

    ts: add typings

---
+++ index.d.ts
@@ -0,0 +1,4 @@
+declare function asde(
+	promise: Promise<any>
+): Promise<[null, any] | [any, undefined]>;
+export default asde;
--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 'use strict';
-function isPromise(obj) {
-	return Boolean(obj) && (typeof obj === 'object' || typeof obj === 'function') && typeof obj.then === 'function';
+function isPromise(object) {
+	return (
+		Boolean(object) &&
+		(typeof object === 'object' || typeof object === 'function') &&
+		typeof object.then === 'function'
+	);
 }
 
-module.exports = p => {
+module.exports = (p) => {
 	if (!isPromise(p)) {
 		throw new TypeError('Expected a promise');
 	}
-	return p.then(res => [null, res]).catch(err => [err]);
+
+	return p.then((result) => [null, result]).catch((error) => [error]);
 };
